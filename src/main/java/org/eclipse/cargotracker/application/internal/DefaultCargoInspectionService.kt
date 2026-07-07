6e5b7b83fe55d0ccbee0ce791e0fa24596e1940d
Oleg.Babichev <oleg.babichev@jetbrains.com>
2026-07-07 11:30:51 +0200

    Convert DefaultCargoInspectionService

--- src/main/java/org/eclipse/cargotracker/application/internal/DefaultCargoInspectionService.kt
+++ src/main/java/org/eclipse/cargotracker/application/internal/DefaultCargoInspectionService.kt
@@ -1,47 +1,50 @@
-package org.eclipse.cargotracker.application.internal;
+package org.eclipse.cargotracker.application.internal
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import jakarta.ejb.Stateless;
-import jakarta.inject.Inject;
-import org.eclipse.cargotracker.application.ApplicationEvents;
-import org.eclipse.cargotracker.application.CargoInspectionService;
-import org.eclipse.cargotracker.domain.model.cargo.Cargo;
-import org.eclipse.cargotracker.domain.model.cargo.CargoRepository;
-import org.eclipse.cargotracker.domain.model.cargo.TrackingId;
-import org.eclipse.cargotracker.domain.model.handling.HandlingEventRepository;
-import org.eclipse.cargotracker.domain.model.handling.HandlingHistory;
+import jakarta.ejb.Stateless
+import jakarta.inject.Inject
+import org.eclipse.cargotracker.application.ApplicationEvents
+import org.eclipse.cargotracker.application.CargoInspectionService
+import org.eclipse.cargotracker.domain.model.cargo.CargoRepository
+import org.eclipse.cargotracker.domain.model.cargo.TrackingId
+import org.eclipse.cargotracker.domain.model.handling.HandlingEventRepository
+import java.util.logging.Level
+import java.util.logging.Logger
 
 @Stateless
-public class DefaultCargoInspectionService implements CargoInspectionService {
+class DefaultCargoInspectionService : CargoInspectionService {
+    @Inject
+    private val logger: Logger? = null
 
-  @Inject private Logger logger;
-  @Inject private ApplicationEvents applicationEvents;
-  @Inject private CargoRepository cargoRepository;
-  @Inject private HandlingEventRepository handlingEventRepository;
+    @Inject
+    private val applicationEvents: ApplicationEvents? = null
 
-  @Override
-  public void inspectCargo(TrackingId trackingId) {
-    Cargo cargo = cargoRepository.find(trackingId);
+    @Inject
+    private val cargoRepository: CargoRepository? = null
 
-    if (cargo == null) {
-      logger.log(Level.WARNING, "Can't inspect non-existing cargo {0}", trackingId);
-      return;
+    @Inject
+    private val handlingEventRepository: HandlingEventRepository? = null
+
+    override fun inspectCargo(trackingId: TrackingId?) {
+        val cargo = cargoRepository!!.find(trackingId)
+
+        if (cargo == null) {
+            logger!!.log(Level.WARNING, "Can't inspect non-existing cargo {0}", trackingId)
+            return
+        }
+
+        val handlingHistory =
+            handlingEventRepository!!.lookupHandlingHistoryOfCargo(trackingId)
+
+        cargo.deriveDeliveryProgress(handlingHistory)
+
+        if (cargo.getDelivery().isMisdirected()) {
+            applicationEvents!!.cargoWasMisdirected(cargo)
+        }
+
+        if (cargo.getDelivery().isUnloadedAtDestination()) {
+            applicationEvents!!.cargoHasArrived(cargo)
+        }
+
+        cargoRepository.store(cargo)
     }
-
-    HandlingHistory handlingHistory =
-        handlingEventRepository.lookupHandlingHistoryOfCargo(trackingId);
-
-    cargo.deriveDeliveryProgress(handlingHistory);
-
-    if (cargo.getDelivery().isMisdirected()) {
-      applicationEvents.cargoWasMisdirected(cargo);
-    }
-
-    if (cargo.getDelivery().isUnloadedAtDestination()) {
-      applicationEvents.cargoHasArrived(cargo);
-    }
-
-    cargoRepository.store(cargo);
-  }
 }
